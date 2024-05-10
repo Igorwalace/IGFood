80b8bf32782aea89bc2a685f636d390d2963b619
igoor <igorwallacy3@gmail.com>
2024-05-10 20:23:13 -0300

    rolagem suavem now

--- src/app/pages/restaurantsRecomendados.tsx
+++ src/app/pages/restaurantsRecomendados.tsx
@@ -12,12 +12,49 @@ const RestaurantsRecomendados = () => {
 
     const handleScroll = () => {
         const element: any = document.getElementById('my-scrollable-element');
-        element.scrollLeft += 300;
-    }
+        const targetScrollPosition = element.scrollLeft + 800; // Posição final da rolagem
+
+        const scrollAmount = 40; // Quantidade de pixels por atualização
+        let currentScrollPosition = element.scrollLeft; // Posição inicial da rolagem
+
+        const animateScroll = () => {
+            if (currentScrollPosition < targetScrollPosition) {
+                currentScrollPosition += scrollAmount;
+                element.scrollLeft = currentScrollPosition;
+
+                // Solicita a próxima atualização do loop
+                requestAnimationFrame(animateScroll);
+            } else {
+                // Parar a animação quando a posição final for atingida
+            }
+        };
+
+        // Iniciar a animação
+        animateScroll();
+    };
     const handleScrollRight = () => {
-        const element: any = document.getElementById('my-scrollable-element');
-        element.scrollLeft -= 300;
-    }
+        const element:any = document.getElementById('my-scrollable-element');
+        const targetScrollPosition = element.scrollLeft - 800; // Target position (scroll left by 300px)
+
+        const scrollAmount = 40; // Number of pixels to scroll per update
+        let currentScrollPosition = element.scrollLeft; // Current scroll position
+
+        const animateScroll = () => {
+            if (currentScrollPosition > targetScrollPosition) {
+                currentScrollPosition -= scrollAmount;
+                element.scrollLeft = currentScrollPosition;
+
+                // Request next animation frame for smooth update
+                requestAnimationFrame(animateScroll);
+            } else {
+                // Stop animation when target position is reached
+                element.scrollLeft = targetScrollPosition; // Ensure final position
+            }
+        };
+
+        // Initiate the animation
+        animateScroll();
+    };
     return (
         <>
             <main>
