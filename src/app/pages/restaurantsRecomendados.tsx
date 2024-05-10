0d79bff98221874e16e5908e04c0a9789d27a7cf
igoor <igorwallacy3@gmail.com>
2024-05-10 20:26:51 -0300

    escondendo as arrow da rolagem caso esteja carregando

--- src/app/pages/restaurantsRecomendados.tsx
+++ src/app/pages/restaurantsRecomendados.tsx
@@ -7,8 +7,11 @@ import PedidosRecomendadosTitle from './componentes/pedidosRecomendadosTitle'
 
 //icons
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
+import useAppContextFirestore from '../contexts/banco'
 
 const RestaurantsRecomendados = () => {
+    const { firestoreRestaurant } = useAppContextFirestore()
+
 
     const handleScroll = () => {
         const element: any = document.getElementById('my-scrollable-element');
@@ -33,7 +36,7 @@ const RestaurantsRecomendados = () => {
         animateScroll();
     };
     const handleScrollRight = () => {
-        const element:any = document.getElementById('my-scrollable-element');
+        const element: any = document.getElementById('my-scrollable-element');
         const targetScrollPosition = element.scrollLeft - 800; // Target position (scroll left by 300px)
 
         const scrollAmount = 40; // Number of pixels to scroll per update
@@ -63,12 +66,17 @@ const RestaurantsRecomendados = () => {
                 </div>
                 <div className='relative' >
                     <Restaurants />
-                    <button className='absolute top-0 bottom-7 left-0 flex justify-center items-center p-2' onClick={handleScrollRight} >
-                        <h1 className='relative text-white bg-[rgb(0,0,0,0.4)] p-1 rounded-xl' ><IoIosArrowBack size={30} /></h1>
-                    </button>
-                    <button className='absolute top-0 bottom-7 right-0 flex justify-center items-center p-2' onClick={handleScroll} >
-                        <h1 className='relative text-white bg-[rgb(0,0,0,0.4)] p-1 rounded-xl' ><IoIosArrowForward size={30} /></h1>
-                    </button>
+                    {
+                        firestoreRestaurant.length != 0 &&
+                        <>
+                            <button className='absolute top-0 bottom-7 left-0 flex justify-center items-center p-2' onClick={handleScrollRight} >
+                                <h1 className='relative text-white bg-[rgb(0,0,0,0.4)] p-1 rounded-xl' ><IoIosArrowBack size={30} /></h1>
+                            </button>
+                            <button className='absolute top-0 bottom-7 right-0 flex justify-center items-center p-2' onClick={handleScroll} >
+                                <h1 className='relative text-white bg-[rgb(0,0,0,0.4)] p-1 rounded-xl' ><IoIosArrowForward size={30} /></h1>
+                            </button>
+                        </>
+                    }
                 </div>
             </main>
         </>
