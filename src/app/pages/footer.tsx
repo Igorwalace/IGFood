2df130acc4e747d954ca6c7fda8e121544cf3f2b
igoor <igorwallacy3@gmail.com>
2024-05-15 11:24:21 -0300

    link att

--- src/app/pages/footer.tsx
+++ src/app/pages/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <>
             <main className='flex items-center justify-between pb-2' >
-                <h1 className='md:text-base text-sm' >Desenvolvido por <Link className='text-[var(--red)] underline hover:scale-105 duration-200' href='https://my-website-igor-eight.vercel.app/' target='_blank' >Igor Walace</Link>.</h1>
+                <h1 className='md:text-base text-sm' >Desenvolvido por <Link className='text-[var(--red)] underline hover:scale-105 duration-200' href='https://meu-website-igor.vercel.app/' target='_blank' >Igor Walace</Link>.</h1>
             </main>
         </>
     )
